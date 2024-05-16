fd0b83a9431fa62bd9ab9822c6faa751ae5e1804
ismaelbenitezbmoll <147537527+ismaelbenitezbmoll@users.noreply.github.com>
2024-05-16 19:04:07 +0200

    Update Jugador.java

--- PokemonBattle/src/dominio/Jugador.java
+++ PokemonBattle/src/dominio/Jugador.java
@@ -34,24 +34,4 @@ public class Jugador {
     public boolean tienePokemon() {
         return !pokemons.isEmpty();
     }
-
-    public void iniciarBatalla(Pokemon pokemonEnemigo) {
-        Scanner sc = new Scanner(System.in);
-        Pokemon pokemon = pokemons.get(0); // Solo tomamos el primer Pokémon del jugador
-        System.out.println("Elige una acción:");
-        System.out.println("1. Ataque básico");
-        System.out.println("2. Ataque de tipo");
-        char opcion = sc.next().charAt(0);
-        switch (opcion) {
-            case '1':
-                pokemon.ataqueBasico(pokemonEnemigo);
-                break;
-            case '2':
-                pokemon.ataqueTipo(pokemonEnemigo);
-                break;
-            default:
-                System.out.println("Selección inválida");
-                break;
-        }
-    }
 }
